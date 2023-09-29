44086fd8a1a74a12b8951259ec782effbdcdd74e
parvizmalik <53339452+parvizmalik@users.noreply.github.com>
2023-09-29 23:03:34 +0400

    Add files via upload
    The `elif` statement is performing two checks:
    
    1. `not stack`: This checks if the `stack` is empty. If the `stack` is empty, `not stack` will evaluate to `True`. This is important because if we're encountering a closing bracket (like `)`, `}`, or `]`) and our stack is empty, it means there was no matching opening bracket for this closing bracket, and the string is not valid.
    
    2. `stack.pop() != c`: This is executed if the `stack` is not empty. `stack.pop()` removes and returns the last element from the `stack`. We're comparing this popped element (which should be the expected matching closing bracket for the last unmatched opening bracket) with the current character `c`. If they are not the same, it means the brackets are not correctly nested, and the string is not valid.
    
    The `elif` condition can be broken down further as:
    
    - If the `stack` is empty (`not stack` is `True`) OR
    - The top of the stack (the last opening bracket we saw) doesn't match with the current closing bracket (`stack.pop() != c`)
    
    ... then return `False`.
    
    In essence, this condition checks for any mismatch or imbalance in the bracket sequence and returns `False` if an issue is detected.

---
+++ solution.py
@@ -0,0 +1,20 @@
+
+
+
+def isValid(s: str) -> bool:
+    stack = []
+    for c in s:
+        if c == '(':
+            stack.append(')')
+        elif c == '{':
+            stack.append('}')
+        elif c == '[':
+            stack.append(']')
+        elif not stack or stack.pop() != c:
+            return False
+    return not stack
+
+
+print(isValid("()"))        # True
+print(isValid("()[]{}"))    # True
+print(isValid("(]"))        # False
